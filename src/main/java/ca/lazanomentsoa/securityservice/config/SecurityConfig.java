34a380066da197854e0e19a207f67bf2a8d64701
Laza_Nomentsoa_Sedera <starksedera@gmail.com>
2024-11-14 20:06:31 -0500

    ajout authentification personnalisee

--- src/main/java/ca/lazanomentsoa/securityservice/config/SecurityConfig.java
+++ src/main/java/ca/lazanomentsoa/securityservice/config/SecurityConfig.java
@@ -10,12 +10,19 @@ import com.nimbusds.jose.proc.SecurityContext;
 import jakarta.servlet.FilterChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -25,20 +32,42 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     private RsakeysConfig rsakeysConfig;
 
-    public SecurityConfig(RsakeysConfig rsakeysConfig) {
+    private PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(RsakeysConfig rsakeysConfig, PasswordEncoder passwordEncoder) {
         this.rsakeysConfig = rsakeysConfig;
+        this.passwordEncoder = passwordEncoder;
     }
 
+    //pour l'authentication avec un utilisateur personnalisé
+    // Cette methode n'est plus appreciée donc il ne faut plus l'utiliser
+    //@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    //pour la nouvelle methode de l'authentication
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService){
+       var authProvider = new DaoAuthenticationProvider();
+       authProvider.setPasswordEncoder(passwordEncoder);
+       authProvider.setUserDetailsService(userDetailsService);
+       return new ProviderManager(authProvider);
+    }
+
+    // ca c'est pour l'authentication basic
+    @Bean
+    public UserDetailsService inMemoryUserDetailsManager() {// on peut l'utiliser pour faire une AuthenticationManager
+    //public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
         return new InMemoryUserDetailsManager(
-                User.withUsername("user1").password("{noop}1234").authorities("USER").build(), //{noop} no password encoder car il le faut
-                User.withUsername("user2").password("{noop}1234").authorities("USER").build(),
-                User.withUsername("admin").password("{noop}1234").authorities("USER","ADMIN").build()
+                User.withUsername("user1").password(passwordEncoder.encode("1234")).authorities("USER").build(), //{noop} no password encoder car il le faut
+               // User.withUsername("user2").password("{noop}1234").authorities("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("1234")).authorities("USER","ADMIN").build()
         );
     }
 
@@ -46,7 +75,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf( csrf -> csrf.disable())
-                .authorizeRequests(auth -> auth.anyRequest().authenticated())
+                //authorize l'url qui n'est pas necessaire dans basic
+                .authorizeHttpRequests(ar -> ar.requestMatchers("/token/**").permitAll())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
                 .sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .httpBasic(Customizer.withDefaults())
--- src/main/java/ca/lazanomentsoa/securityservice/web/TestRestAPI.java
+++ src/main/java/ca/lazanomentsoa/securityservice/web/TestRestAPI.java
@@ -4,6 +4,7 @@ package ca.lazanomentsoa.securityservice.web;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -12,6 +13,7 @@ import java.util.Map;
 public class TestRestAPI {
 
     @GetMapping("/dataTest")
+    @PreAuthorize("hasAuthority('SCOPE_USER')") // pour activer @PreAuthorize, il faut ajouter dans  SecurityConfig @EnableGlobalMethodSecurity
     public Map<String, Object> dataTest(Authentication authentication){
         return Map.of(
                 "message", "Data test",
@@ -19,4 +21,9 @@ public class TestRestAPI {
                 "authorities", authentication.getAuthorities());
     }
 
+    @PostMapping("/saveData")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    public Map<String, Object> saveData(Authentication authentication, String data){
+        return Map.of("dataSaved", data);
+    }
 }
